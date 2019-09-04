722c83c4683d0e59d2171bae14e6d43401dfc349
p00rt <serzhooo@gmail.com>
2019-09-04 17:16:34 +0300

    02: Add route components

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return (
+    <View>  
+        <Text>In future, this pages will be search</Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default SearchScreen;
+
+
+
